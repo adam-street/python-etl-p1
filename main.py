85af7abc0e656961ce9699224361b772a25614d3
Adam Street <adam.street.dev@gmail.com>
2021-12-02 22:27:42 -0600

    updated query example to be more clear

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pgsql import query
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("results: ", query(sql.test_query))
+    results = query(sql.test_query);
+    print("results: ", results)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
