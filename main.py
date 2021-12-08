1f46753d4cd6c3fa79d7294ebe1dd3d89b1e669c
Pmoua015 <paul.moua2014@gmail.com>
2021-12-08 09:35:39 -0600

    commit petl1 schema tables and data

--- main.py
+++ main.py
@@ -1,13 +1,113 @@
 import sql
 from pgsql import query
+import csv
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
-    # insert data
-    query(sql.test_insert, ["My Insert!"]);
+
+    # query(sql.create_schema)
+    query(sql.annual_ticket_sales_table, [""])
+    query(sql.highest_grossers_table, [""])
+    query(sql.popular_creative_types_table, [""])
+    query(sql.top_distributors_table, [""])
+    query(sql.top_genres_table, [""])
+    query(sql.top_grossing_ratings_table, [""])
+    query(sql.top_grossing_sources_table, [""])
+    query(sql.top_production_methods_table, [""])
+    query(sql.wide_releases_count_table, [""])
+
 
     # select data
-    results = query(sql.test_select);
-    print("results: ", results)
+    # results = query(sql.test_select);
+    #print("results: ", results)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with open('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/AnnualTicketSales.csv',
+              newline='', encoding='utf-8-sig') as csvfile:
+        reader = csv.reader(csvfile)
+        i = 0
+        for row in reader:
+            row.pop()
+            if i > 0:
+                query(sql.insert_row1, row)
+            i += 1
+
+    with open('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/HighestGrossers.csv',
+              newline='', encoding='utf-8-sig') as csvfile:
+        reader = csv.reader(csvfile)
+        i = 0
+        for row in reader:
+            # row.pop()
+            if i > 0:
+                query(sql.insert_row2, row)
+            i += 1
+
+    with open('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/PopularCreativeTypes.csv',
+            newline='', encoding='utf-8-sig') as csvfile:
+        reader = csv.reader(csvfile)
+        i = 0
+        for row in reader:
+            #row.pop()
+            if i > 0:
+                query(sql.insert_row3, row)
+            i +=1
+
+with open('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/TopDistributors.csv',
+       newline='', encoding='utf-8-sig') as csvfile:
+   reader = csv.reader(csvfile)
+   i = 0
+   for row in reader:
+       #row.pop()
+       if i > 0:
+           query(sql.insert_row4, row)
+       i +=1
+
+with open('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/TopGenres.csv',
+       newline='', encoding='utf-8-sig') as csvfile:
+   reader = csv.reader(csvfile)
+   i = 0
+   for row in reader:
+       #row.pop()
+       if i > 0:
+           query(sql.insert_row5, row)
+       i +=1
+
+with open('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/TopGrossingRatings.csv',
+       newline='', encoding='utf-8-sig') as csvfile:
+   reader = csv.reader(csvfile)
+   i = 0
+   for row in reader:
+       #row.pop()
+       if i > 0:
+           query(sql.insert_row6, row)
+       i +=1
+
+with open('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/TopGrossingSources.csv',
+       newline='', encoding='utf-8-sig') as csvfile:
+   reader = csv.reader(csvfile)
+   i = 0
+   for row in reader:
+       #row.pop()
+       if i > 0:
+           query(sql.insert_row7, row)
+       i +=1
+
+with open('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/TopProductionMethods.csv',
+       newline='', encoding='utf-8-sig') as csvfile:
+   reader = csv.reader(csvfile)
+   i = 0
+   for row in reader:
+       #row.pop()
+       if i > 0:
+           query(sql.insert_row8, row)
+       i +=1
+
+with open('datasets/hollywood-theatrical-market-synopsis-1995-to-2021/WideReleasesCount.csv',
+       newline='', encoding='utf-8-sig') as csvfile:
+   reader = csv.reader(csvfile)
+   i = 0
+   for row in reader:
+       row.pop()
+       if i > 0:
+           query(sql.insert_row9, row)
+       i +=1
