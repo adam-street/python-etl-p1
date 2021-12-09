14a17133315137a4fe7d9fc167b0b0bb9a215c78
Nadhiya-Professional <nadhiya.b@gmail.com>
2021-12-09 15:07:15 -0600

    Automated_Version

--- main.py
+++ main.py
@@ -1,35 +1,130 @@
 import sql
 from pgsql import query
-from pgsql import query_select
 import csv
+from pgsql import query_select
 
-
-def parsing_csv():
-    i = 0
-    with open(".//datasets//hollywood-theatrical-market-synopsis-1995-to-2021//PopularCreativeTypes.csv", newline='',
-              encoding='utf-8-sig') as f:
+def parsing_csv1(file_name):
+    query(sql.test_create1)
+    with open(file_name, newline='', encoding='utf-8-sig') as f:
+        j = 0
         reader = csv.reader(f)
         for row in reader:
-            # row.pop()
-            if i > 0:
-                query(sql.test_insert, row)
-            i += 1
+            row.pop()
+            if j > 0:
+                query(sql.test_insert1, row)
+            j += 1
 
 
-# Press the green button in the gutter to run the script.
+def parsing_csv2(file_name):
+    query(sql.test_create2)
+    with open(file_name, newline='', encoding='utf-8-sig') as f:
+        j = 0
+        reader = csv.reader(f)
+        for row in reader:
+            if j > 0:
+                query(sql.test_insert2, row)
+            j += 1
+
+
+def parsing_csv3(file_name):
+    query(sql.test_create3)
+    with open(file_name, newline='', encoding='utf-8-sig') as f:
+        reader = csv.reader(f)
+        row_count = sum(1 for row in reader)
+
+    with open(file_name, newline='', encoding='utf-8-sig') as f:
+        reader = csv.reader(f)
+        j = 0
+        for row in reader:
+            if j > 0:
+                if j < row_count - 1:
+                    query(sql.test_insert3, row)
+            j += 1
+
+
+def parsing_csv4(file_name):
+    query(sql.test_create4)
+    with open(file_name, newline='', encoding='utf-8-sig') as f:
+        j = 0
+        reader = csv.reader(f)
+        for row in reader:
+            if j > 0:
+                query(sql.test_insert4, row)
+            j += 1
+
+
+def parsing_csv5(file_name):
+    query(sql.test_create5)
+    with open(file_name, newline='', encoding='utf-8-sig') as f:
+        j = 0
+        reader = csv.reader(f)
+        for row in reader:
+            if j > 0:
+                query(sql.test_insert5, row)
+            j += 1
+
+
+def parsing_csv6(file_name):
+    query(sql.test_create6)
+    with open(file_name, newline='', encoding='utf-8-sig') as f:
+        j = 0
+        reader = csv.reader(f)
+        for row in reader:
+            if j > 0:
+                query(sql.test_insert6, row)
+            j += 1
+
+
+def parsing_csv7(file_name):
+    query(sql.test_create7)
+    with open(file_name, newline='', encoding='utf-8-sig') as f:
+        j = 0
+        reader = csv.reader(f)
+        for row in reader:
+            if j > 0:
+                query(sql.test_insert7, row)
+            j += 1
+
+def parsing_csv8(file_name):
+    query(sql.test_create8)
+    with open(file_name, newline='', encoding='utf-8-sig') as f:
+        j = 0
+        reader = csv.reader(f)
+        for row in reader:
+            if j > 0:
+                query(sql.test_insert8, row)
+            j += 1
+
+
+def parsing_csv9(file_name):
+    query(sql.test_create9)
+    with open(file_name, newline='', encoding='utf-8-sig') as f:
+        j = 0
+        reader = csv.reader(f)
+        for row in reader:
+            row.pop()
+            if j > 0:
+                query(sql.test_insert9, row)
+            j += 1
+
 
 if __name__ == '__main__':
     # insert data
     query(sql.create_schema)
 
-    query(sql.test_create)
-
     # parsing file
-
-    parsing_csv()
+    parsing_csv1(".//datasets/hollywood-theatrical-market-synopsis-1995-to-2021/AnnualTicketSales.csv")
+    parsing_csv2(".//datasets//hollywood-theatrical-market-synopsis-1995-to-2021//HighestGrossers.csv")
+    parsing_csv3(".//datasets//hollywood-theatrical-market-synopsis-1995-to-2021//PopularCreativeTypes.csv")
+    parsing_csv4(".//datasets//hollywood-theatrical-market-synopsis-1995-to-2021//TopDistributors.csv")
+    parsing_csv5(".//datasets//hollywood-theatrical-market-synopsis-1995-to-2021//TopGenres.csv")
+    parsing_csv6(".//datasets//hollywood-theatrical-market-synopsis-1995-to-2021//TopGrossingRatings.csv")
+    parsing_csv7(".//datasets//hollywood-theatrical-market-synopsis-1995-to-2021//TopGrossingSources.csv")
+    parsing_csv8(".//datasets//hollywood-theatrical-market-synopsis-1995-to-2021//TopProductionMethods.csv")
+    parsing_csv9(".//datasets//hollywood-theatrical-market-synopsis-1995-to-2021//WideReleasesCount.csv")
 
     # select data
     results = query_select(sql.test_select);
     print("results: ", results)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
--- sql.py
+++ sql.py
@@ -1,9 +1,43 @@
 create_schema = ('''
-    create schema IF NOT EXISTS Nads;
+    create schema IF NOT EXISTS petl1;
 ''')
 
-test_create = ('''
-    CREATE TABLE if not exists Nads.TopDistributors (
+test_create1 = ('''
+    CREATE TABLE if not exists petl1.AnnualTicketSales (
+        _Year_ varchar NOT NULL,
+        Tickets_Sold varchar,
+        Total_Box_Office varchar,
+        Total_Inflated_Adjusted_Box_Office varchar,
+        Avergae_Ticket_Price varchar
+    )
+    ''')
+
+test_create2 = ('''
+    CREATE TABLE if not exists petl1.HighestGrossers (
+        Year varchar NOT NULL,
+        Movie varchar,
+        Genre varchar,
+        MPAA_Rating varchar,
+        Distributor varchar,
+        Total_For_Year varchar,
+        Total_in_2019_dollars varchar,
+        Tickets_Sold varchar
+    )
+    ''')
+
+test_create3 = ('''
+    CREATE TABLE if not exists petl1.PopularCreativeTypes (
+        Rank varchar NOT NULL,
+        Creative_types varchar,
+        Movies varchar,
+        Total_gross varchar,
+        Avergae_gross varchar,
+        Market_share varchar
+    )
+    ''')
+
+test_create4 = ('''
+    CREATE TABLE if not exists petl1.TopDistributors (
         Rank varchar NOT NULL,
         distributors varchar,
         Movies varchar,
@@ -13,13 +47,110 @@ test_create = ('''
     )
     ''')
 
+test_create5 = ('''
+    CREATE TABLE if not exists petl1.TopGenres (
+        Rank varchar NOT NULL,
+        Genres varchar,
+        Movies varchar,
+        Total_gross varchar,
+        Avergae_gross varchar,
+        Market_share varchar
+    )
+    ''')
 
-test_insert = ('''
-  INSERT INTO Nads.TopDistributors
+test_create6 = ('''
+    CREATE TABLE if not exists petl1.TopGrossingRatings (
+        Rank varchar NOT NULL,
+        MPAA_Ratings varchar,
+        Movies varchar,
+        Total_gross varchar,
+        Avergae_gross varchar,
+        Market_share varchar
+    )
+    ''')
+
+test_create7 = ('''
+    CREATE TABLE if not exists petl1.TopGrossingSources (
+        Rank varchar NOT NULL,
+        Sources varchar,
+        Movies varchar,
+        Total_gross varchar,
+        Avergae_gross varchar,
+        Market_share varchar
+    )
+    ''')
+
+test_create8 = ('''
+    CREATE TABLE if not exists petl1.TopProductionMethods (
+        Rank varchar NOT NULL,
+        Production_Methods varchar,
+        Movies varchar,
+        Total_gross varchar,
+        Average_gross varchar,
+        Market_share varchar
+    )
+    ''')
+
+test_create9 = ('''
+    CREATE TABLE if not exists petl1.WideReleasesCount (
+        _Year_ int NOT NULL,
+        Warner_Bros int,
+        Walt_Disney int,
+        _20th_Century_Fox int,
+        Paramount_Pictures int,
+        Sony_Pictures int,
+        Universal int,
+        Total_Major_6 int,
+        Total_Other_Studios int
+        
+    )
+    ''')
+
+test_insert1 = ('''
+  INSERT INTO petl1.AnnualTicketSales
+  VALUES(%s,%s,%s,%s,%s);
+''')
+
+test_insert2 = ('''
+  INSERT INTO petl1.HighestGrossers
+  VALUES(%s,%s,%s,%s,%s,%s,%s,%s);
+''')
+
+test_insert3 = ('''
+  INSERT INTO petl1.PopularCreativeTypes
   VALUES(%s,%s,%s,%s,%s,%s);
 ''')
 
+test_insert4 = ('''
+  INSERT INTO petl1.TopDistributors
+  VALUES(%s,%s,%s,%s,%s,%s);
+''')
+
+test_insert5 = ('''
+  INSERT INTO petl1.TopGenres
+  VALUES(%s,%s,%s,%s,%s,%s);
+''')
+
+test_insert6 = ('''
+  INSERT INTO petl1.TopGrossingRatings
+  VALUES(%s,%s,%s,%s,%s,%s);
+''')
+
+test_insert7 = ('''
+  INSERT INTO petl1.TopGrossingSources
+  VALUES(%s,%s,%s,%s,%s,%s);
+''')
+
+test_insert8 = ('''
+  INSERT INTO petl1.TopProductionMethods
+  VALUES(%s,%s,%s,%s,%s,%s);
+''')
+
+test_insert9 = ('''
+  INSERT INTO petl1.WideReleasesCount
+  VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s);
+''')
 test_select = ('''
   SELECT *
-  FROM Nads.TopDistributors
+  FROM petl1.WideReleasesCount
 ''')
